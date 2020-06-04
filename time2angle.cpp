8539749c606962a7b95ec42c3907589238028d9e
Petr Semiletov <tea@list.ru>
2020-06-04 13:23:47 +0300

    timediff upd

--- time2angle.cpp
+++ time2angle.cpp
@@ -31,6 +31,6 @@ int main (int argc, char *argv[])
 
   cout << A << "°" << B << "\'" << C << "\"" << endl;
   cout << "В секундах дуги:" << S << endl;
-  
+
   return 0;
 }
--- timediff.cpp
+++ timediff.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <ctime>
 #include <iomanip>
 
@@ -6,13 +7,21 @@ using namespace std;
 
 int main (int argc, char *argv[])
 {
+  if (argc < 1)
+     {     
+      cout << "time diff params must be hh:mm:ss hh:mm:ss" << endl;
+      return 0;
+     }
+
+
   std::tm tm1 = {};
   std::tm tm2 = {};
 
-  cout << "time 1:" << endl;
-  cin >> get_time (&tm1, "%H:%M:%S");
-  cout << "time 2:" << endl;
-  cin >> get_time (&tm2, "%H:%M:%S");
+  std::istringstream s1 (argv[1]);
+  std::istringstream s2 (argv[2]);
+
+  s1 >> get_time (&tm1, "%H:%M:%S");
+  s2 >> get_time (&tm2, "%H:%M:%S");
 
   std::time_t t1 = mktime(&tm1);
   std::time_t t2 = mktime(&tm2);
